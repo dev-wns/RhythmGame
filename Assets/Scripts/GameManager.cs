5ad5ef7ec34e7f9ad0867dfd5915e3937ec53975
wnsghks <ashi0@naver.com>
2021-08-19 02:22:04 +0900

    [Editor] 셀렉트 씬 곡 정보 UI 작업

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static Dictionary<string /* sound name */, Sound> soundList = new Dictionary<string, Sound>();
+
+    private void Awake()
+    {
+        DontDestroyOnLoad( this.gameObject );
+    }
 }
