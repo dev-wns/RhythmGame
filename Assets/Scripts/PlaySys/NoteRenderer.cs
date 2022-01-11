24ef956cde6d46f6c994b939072cf1cc5b0946d8
wnsghks <ashi0@naver.com>
2022-01-12 07:04:59 +0900

    노트 Head, Body, Tail로 나누어 렌더링

--- Assets/Scripts/PlaySys/NoteRenderer.cs
+++ Assets/Scripts/PlaySys/NoteRenderer.cs
@@ -6,6 +6,8 @@ public class NoteRenderer : MonoBehaviour
 {
     private NoteSystem system;
 
+    public GameObject head, body, tail;
+
     public float Time { get; private set; }
     public float CalcTime { get; private set; }
     public float SliderTime { get; private set; }
@@ -32,26 +34,41 @@ public class NoteRenderer : MonoBehaviour
         column = GlobalSetting.NoteStartPos + ( _lane * GlobalSetting.NoteWidth ) + ( ( _lane + 1 ) * GlobalSetting.NoteBlank );
 
         if ( IsSlider )
+        {
             system.CurrentScene.OnScrollChanged += ScaleUpdate;
+            body.SetActive( true );
+            tail.SetActive( true );
+        }
+        else
+        {
+            body.SetActive( false );
+            tail.SetActive( false );
+        }
 
         ScaleUpdate();
-        if ( _lane == 1 || _lane == 4 ) SetColor( new Color( 0.2078432f, 0.7843138f, 1f, 1f ) );
-        else                            SetColor( Color.white );
+        if ( _lane == 1 || _lane == 4 ) head.GetComponent<SpriteRenderer>().color = new Color( 0.2078432f, 0.7843138f, 1f, 1f );
+        else                            head.GetComponent<SpriteRenderer>().color = Color.white;
     }
 
     public void SetColor( Color _color ) => rdr.color = _color;
 
     private void ScaleUpdate()
     {
-        if ( IsSlider ) transform.localScale = new Vector3( GlobalSetting.NoteWidth, Mathf.Abs( ( CalcSliderTime - CalcTime ) * GameSetting.Weight ), 1f );
-        else            transform.localScale = new Vector3( GlobalSetting.NoteWidth, GlobalSetting.NoteHeight, 1f );
+        head.transform.localScale = new Vector3( GlobalSetting.NoteWidth, GlobalSetting.NoteHeight, 1f );
+        if ( IsSlider )
+        {
+            body.transform.localScale = new Vector3( GlobalSetting.NoteWidth * .8f, Mathf.Abs( ( CalcSliderTime - CalcTime ) * GameSetting.Weight ), 1f );
+            tail.transform.localScale = new Vector3( GlobalSetting.NoteWidth, GlobalSetting.NoteHeight, 1f );
+        }
+
+        // if ( IsSlider ) transform.localScale = new Vector3( GlobalSetting.NoteWidth, Mathf.Abs( ( CalcSliderTime - CalcTime ) * GameSetting.Weight ), 1f );
+        // else            transform.localScale = new Vector3( GlobalSetting.NoteWidth, GlobalSetting.NoteHeight, 1f );
     }
 
     private void Awake()
     {
-        rdr   = GetComponent<SpriteRenderer>();
+        rdr = GetComponent<SpriteRenderer>();
     }
-
 
     public void Despawn()
     {
@@ -63,22 +80,30 @@ public class NoteRenderer : MonoBehaviour
 
     private void LateUpdate()
     {
-        if ( isHolding )
+        var moveTime = GlobalSetting.JudgeLine + ( ( newTime - NowPlaying.PlaybackChanged ) * GameSetting.Weight );
+        head.transform.position = new Vector3( column, moveTime, 2f );
+        if ( IsSlider )
         {
-            float startDiff = ( CalcTime       - NowPlaying.PlaybackChanged ) * GameSetting.Weight;
-            float endDiff   = ( CalcSliderTime - NowPlaying.PlaybackChanged ) * GameSetting.Weight;
-            float currentScale = Mathf.Abs( endDiff - startDiff ) - Mathf.Abs( startDiff );
-            if ( endDiff > 0 && transform.localScale.y > 0 )
-                 transform.localScale = new Vector3( GlobalSetting.NoteWidth, currentScale, 1f );
-            else
-                 transform.localScale = new Vector3( GlobalSetting.NoteWidth, 0f, 1f );
+            body.transform.position = new Vector3( column, moveTime, 2f );
+            tail.transform.position = new Vector3( column, Mathf.Abs( ( CalcSliderTime - CalcTime ) * GameSetting.Weight ) + moveTime, 2f );
+        }
 
-            transform.position = new Vector3( column, GlobalSetting.JudgeLine, 2f );
-            newTime = NowPlaying.PlaybackChanged;
-        }
-        else
-        {
-            transform.position = new Vector3( column, GlobalSetting.JudgeLine + ( ( newTime - NowPlaying.PlaybackChanged ) * GameSetting.Weight ), 2f );
-        }
+        //if ( isHolding )
+        //{
+        //    float startDiff = ( CalcTime       - NowPlaying.PlaybackChanged ) * GameSetting.Weight;
+        //    float endDiff   = ( CalcSliderTime - NowPlaying.PlaybackChanged ) * GameSetting.Weight;
+        //    float currentScale = Mathf.Abs( endDiff - startDiff ) - Mathf.Abs( startDiff );
+        //    if ( endDiff > 0 && transform.localScale.y > 0 )
+        //         transform.localScale = new Vector3( GlobalSetting.NoteWidth, currentScale, 1f );
+        //    else
+        //         transform.localScale = new Vector3( GlobalSetting.NoteWidth, 0f, 1f );
+
+        //    transform.position = new Vector3( column, GlobalSetting.JudgeLine, 2f );
+        //    newTime = NowPlaying.PlaybackChanged;
+        //}
+        //else
+        //{
+        //    transform.position = new Vector3( column, GlobalSetting.JudgeLine + ( ( newTime - NowPlaying.PlaybackChanged ) * GameSetting.Weight ), 2f );
+        //}
     }
 }
