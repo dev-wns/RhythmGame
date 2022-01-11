2732f0e3683711133a985fa2ccbf890e71064376
wnsghks <ashi0@naver.com>
2022-01-11 12:38:42 +0900

    NoSliderMod 기능 제작 ( 슬라이더의 시작 부분이 일반노트로 바뀝니다. )

--- Assets/Scripts/Scenes/InGame.cs
+++ Assets/Scripts/Scenes/InGame.cs
@@ -16,7 +16,7 @@ public class InGame : Scene
 
     public List<NoteSystem>  noteSystems  = new List<NoteSystem>();
     public MeasureSystem measureSystem;
-
+    private Chart chart;
     float delta;
 
     private struct CalcNote
@@ -28,146 +28,14 @@ public class InGame : Scene
 
     private void Start()
     {
-        var chart = NowPlaying.Inst.CurrentChart;
-
-        // Notes
-        var notes = chart.notes;
-        CalcNote[] column = new CalcNote[6];
+        chart = NowPlaying.Inst.CurrentChart;
         Random.InitState( ( int )System.DateTime.Now.Ticks );
-        for ( int i = 0; i < notes.Count; i++ )
-        {
-            // Note newNote = new Note( notes[i].line, notes[i].time, notes[i].calcTime, 0, 0, false );
-            switch ( GameSetting.CurrentRandom )
-            {
-                case GameRandom.None:
-                case GameRandom.Mirror:
-                case GameRandom.Random:
-                case GameRandom.Half_Random:
-                     noteSystems[notes[i].line].AddNote( notes[i] );
-                break;
-
-                case GameRandom.Max_Random:
-                {
-                    int count = -1;
-                    for ( int j = 0; j < 6; j++ )
-                    {
-                        if ( i + j < notes.Count && notes[i].time == notes[i + j].time )
-                        {
-                            column[notes[i + j].line].note     = notes[i + j];
-                            column[notes[i + j].line].noteTime = notes[i + j].time;
-
-                            if ( notes[i + j].isSlider )
-                            {
-                                column[notes[i + j].line].sliderTime = notes[i + j].sliderTime;
-                            }
-
-                            count++;
-                        }
-                        else break;
-                    }
-                    i += count;
-
-                    for ( int j = 0; j < 6; j++ )
-                    {
-                        var rand = Random.Range( 0, 5 );
-
-                        bool isOverlab = false;
-                        for( int k = 0; k < 6; k++ )
-                        {
-                            if ( column[k].sliderTime > column[rand].noteTime )
-                            {
-                                isOverlab = true;
-                                break;
-                            }
-                        }
-
-                        if ( !isOverlab )
-                        {
-                            var tmp = column[j];
-                            column[j] = column[rand];
-                            column[rand] = tmp;
-                        }
-                    }
-
-                    for ( int j = 0; j < 6; j++ )
-                    {
-                        if ( column[j].note.HasValue )
-                             noteSystems[j].AddNote( column[j].note.Value );
-
-                        column[j].note = null;
-                    }
-                }
-                break;
-            }
-        }
-        switch ( GameSetting.CurrentRandom )
-        {
-            case GameRandom.Mirror:
-                 noteSystems.Reverse();
-            break;
-
-            case GameRandom.Random:
-            {
-                for ( int i = 0; i < 6; i++ )
-                {
-                    var rand = Random.Range( 0, 5 );
-
-                    var tmp = noteSystems[i];
-                    noteSystems[i]    = noteSystems[rand];
-                    noteSystems[rand] = tmp;
-                }
-            } break;
-
-            case GameRandom.Half_Random:
-            {
-                for ( int i = 0; i < 6; i++ )
-                {
-                    var rand = Random.Range( 0, 2 );
-
-                    var tmp = noteSystems[i];
-                    noteSystems[i] = noteSystems[rand];
-                    noteSystems[rand] = tmp;
-                }
-
-                for ( int i = 2; i < 6; i++ )
-                {
-                    var rand = Random.Range( 2, 5 );
-
-                    var tmp = noteSystems[i];
-                    noteSystems[i] = noteSystems[rand];
-                    noteSystems[rand] = tmp;
-                }
-            } break;
-        }
+        
+        CreateNotes();
+        CreateMeasures();
 
         for ( int i = 0; i < 6; i++ )
               noteSystems[i].lane = i;
-
-
-        // Measures
-        var timings = chart.timings;
-        for ( int i = 0; i < timings.Count; i++ )
-        {
-            float time;
-            Timing timing = timings[i];
-
-            if ( timing.bpm < 10 )
-                continue;
-            float bpms = ( timing.bpm / 60f ) * 1000f / 4; // beat per milliseconds
-
-            if ( i + 1 == timings.Count )
-                time = chart.notes[chart.notes.Count - 1].time;
-            else
-                time = timings[i + 1].time;
-
-            int maxCount = Mathf.FloorToInt( ( time - timing.time ) / bpms );
-            measureSystem.AddTime( NowPlaying.GetChangedTime( timing.time ) );
-
-            for ( int j = 1; j < maxCount + 1; j++ )
-            {
-                measureSystem.AddTime( NowPlaying.GetChangedTime( timing.time + ( j * bpms ) ) );
-            }
-        }
 
         OnGameStart();
         StartCoroutine( BpmChnager() );
@@ -194,6 +62,157 @@ public class InGame : Scene
         frameText.text = string.Format( "{0:F1}", 1f / delta );
 
         //comboText.text = string.Format( "{0}", GameManager.Combo );
+    }
+
+    private void CreateNotes()
+    {
+        var notes = chart.notes;
+        CalcNote[] column = new CalcNote[6];
+
+        for ( int i = 0; i < notes.Count; i++ )
+        {
+            bool hasNoSliderMod = GameSetting.CurrentGameMod.HasFlag( GameMod.NoSlider );
+            
+            switch ( GameSetting.CurrentRandom )
+            {
+                case GameRandom.None:
+                case GameRandom.Mirror:
+                case GameRandom.Random:
+                case GameRandom.Half_Random:
+                {
+                    Note newNote;
+                    if ( hasNoSliderMod ) newNote = new Note( notes[i].line, notes[i].time, notes[i].calcTime, 0, 0, false );
+                    else                  newNote = notes[i];
+                    noteSystems[notes[i].line].AddNote( newNote );
+                } break;
+
+                case GameRandom.Max_Random:
+                {
+                    int count = -1;
+                    // 타격시간이 같은 노트 저장
+                    for ( int j = 0; j < 6; j++ )
+                    {
+                        if ( i + j < notes.Count && notes[i].time == notes[i + j].time )
+                        {
+                            column[notes[i + j].line].note     = notes[i + j];
+                            column[notes[i + j].line].noteTime = notes[i + j].time;
+
+                            if ( !hasNoSliderMod && notes[i + j].isSlider )
+                            {
+                                column[notes[i + j].line].sliderTime = notes[i + j].sliderTime;
+                            }
+
+                            count++;
+                        }
+                        else break;
+                    }
+                    i += count;
+
+                    // 일반노트만 있을 때 스왑
+                    for ( int j = 0; j < 6; j++ )
+                    {
+                        var rand = Random.Range( 0, 5 );
+
+                        bool isOverlab = false;
+                        for ( int k = 0; k < 6; k++ )
+                        {
+                            if ( column[k].sliderTime >= column[rand].noteTime )
+                            {
+                                isOverlab = true;
+                                break;
+                            }
+                        }
+
+                        if ( !isOverlab )
+                        {
+                            var tmp = column[j];
+                            column[j] = column[rand];
+                            column[rand] = tmp;
+                        }
+                    }
+
+                    // 노트 추가
+                    for ( int j = 0; j < 6; j++ )
+                    {
+                        if ( column[j].note.HasValue )
+                        {
+                            Note newNote;
+                            Note note = column[j].note.Value;
+                            if ( hasNoSliderMod ) newNote = new Note( note.line, note.time, note.calcTime, 0, 0, false );
+                            else                  newNote = note;
+                            noteSystems[j].AddNote( newNote );
+                        }
+
+                        column[j].note = null;
+                    }
+                } break;
+            }
+        }
+
+        // 라인별 스왑일 때
+        switch ( GameSetting.CurrentRandom )
+        {
+            case GameRandom.Mirror:
+            noteSystems.Reverse();
+            break;
+
+            case GameRandom.Random:
+            {
+                for ( int i = 0; i < 6; i++ )
+                {
+                    var rand = Random.Range( 0, 5 );
+
+                    var tmp = noteSystems[i];
+                    noteSystems[i] = noteSystems[rand];
+                    noteSystems[rand] = tmp;
+                }
+            } break;
+
+            case GameRandom.Half_Random:
+            {
+                for ( int i = 0; i < 6; i++ )
+                {
+                    var rand = Random.Range( 0, 2 );
+
+                    var tmp = noteSystems[i];
+                    noteSystems[i] = noteSystems[rand];
+                    noteSystems[rand] = tmp;
+                }
+
+                for ( int i = 2; i < 6; i++ )
+                {
+                    var rand = Random.Range( 2, 5 );
+
+                    var tmp = noteSystems[i];
+                    noteSystems[i] = noteSystems[rand];
+                    noteSystems[rand] = tmp;
+                }
+            } break;
+        }
+    }
+
+    private void CreateMeasures()
+    {
+        var timings = chart.timings;
+        for ( int i = 0; i < timings.Count; i++ )
+        {
+            float time;
+            Timing timing = timings[i];
+
+            if ( timing.bpm < 10 ) continue;
+            float bpms = ( timing.bpm / 60f ) * 1000f / 4; // beat per milliseconds
+
+            if ( i + 1 == timings.Count ) time = chart.notes[chart.notes.Count - 1].time;
+            else                          time = timings[i + 1].time;
+
+            int maxCount = Mathf.FloorToInt( ( time - timing.time ) / bpms );
+            measureSystem.AddTime( NowPlaying.GetChangedTime( timing.time ) );
+
+            for ( int j = 1; j < maxCount + 1; j++ )
+            {
+                measureSystem.AddTime( NowPlaying.GetChangedTime( timing.time + ( j * bpms ) ) );
+            }
+        }
     }
 
     public override void KeyBind()
