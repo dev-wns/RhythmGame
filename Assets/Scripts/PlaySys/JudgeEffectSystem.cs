8fc45f587719b4c108d1a2f9e702fc919800438e
wnsghks <ashi0@naver.com>
2022-01-21 22:39:43 +0900

    Judge Effect 이미지 이동, 페이드 효과 추가

--- Assets/Scripts/PlaySys/JudgeEffectSystem.cs
+++ Assets/Scripts/PlaySys/JudgeEffectSystem.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class JudgeEffectSystem : NumberAtlasBase
 {
     private Judgement judge;
     private SpriteRenderer rdr;
+    private Sequence moveHideSequence;
+    private Vector3 initPosCache;
 
     protected override void Awake()
     {
@@ -14,14 +17,29 @@ public class JudgeEffectSystem : NumberAtlasBase
         judge = GameObject.FindGameObjectWithTag( "Judgement" ).GetComponent<Judgement>();
         judge.OnJudge += HitEffect;
 
-        rdr.color = Color.clear;
+        initPosCache = transform.position;
     }
 
+    private void Start()
+    {
+        moveHideSequence = DOTween.Sequence();
+
+        moveHideSequence.Pause().SetAutoKill( false );
+        moveHideSequence.AppendCallback( () => transform.position = initPosCache );
+        moveHideSequence.Append( transform.DOMoveY( initPosCache.y + 30f, .15f ) );
+        moveHideSequence.AppendInterval( .5f );
+        moveHideSequence.Append( rdr.DOFade( 0f, .5f ) );
+    }
+
+    private void OnDestroy()
+    {
+        moveHideSequence.Kill();
+    }
     private void HitEffect( JudgeType _type )
     {
         switch ( _type )
         {
-            case JudgeType.None:                                 break;
+            case JudgeType.None:                                 return;
             case JudgeType.Perfect:     rdr.sprite = sprites[5]; break;
             case JudgeType.LazyPerfect: rdr.sprite = sprites[4]; break;
             case JudgeType.Great:       rdr.sprite = sprites[3]; break;
@@ -31,5 +49,6 @@ public class JudgeEffectSystem : NumberAtlasBase
         }
 
         rdr.color = Color.white;
+        moveHideSequence.Restart();
     }
 }
