6c74ecee10c7df0de6884e0227630cd0fac25042
wnsghks <ashi0@naver.com>
2022-01-29 04:51:03 +0900

    InGame 시간 표시 ( 서클UI 반시계 방향으로 노래 재생시간에 맞춰 색 채우도록 함 )

---
+++ Assets/Scripts/UISys/FilledTime.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FilledTime : MonoBehaviour
+{
+    public Image image;
+
+    private InGame game;
+    private double length;
+
+    private void Awake()
+    {
+        game = GameObject.FindGameObjectWithTag( "Scene" ).GetComponent<InGame>();
+        game.OnGameStart += Initialize;
+    }
+
+    private void Initialize()
+    {
+        length = NowPlaying.Inst.CurrentSong.totalTime * .001d;
+    }
+
+    private void Update()
+    {
+        image.fillAmount = ( float )( NowPlaying.Playback / length );
+    }
+}
