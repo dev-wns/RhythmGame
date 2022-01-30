d9564345b6dd0066ebc28ec0942dcb65679e45e6
wnsghks <ashi0@naver.com>
2022-01-31 01:07:07 +0900

    InGame Background Image 생성

---
+++ Assets/Scripts/UISys/SongBackground.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class SongBackground : MonoBehaviour
+{
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        StartCoroutine( LoadBackground( NowPlaying.Inst.CurrentSong.imagePath ) );
+    }
+
+    private void OnDestroy()
+    {
+        if ( image.sprite )
+        {
+            if ( image.sprite.texture )
+            {
+                DestroyImmediate( image.sprite.texture );
+            }
+            Destroy( image.sprite );
+        }
+    }
+
+    public IEnumerator LoadBackground( string _path )
+    {
+        bool isExist = System.IO.File.Exists( _path );
+        if ( isExist )
+        {
+            using ( UnityWebRequest www = UnityWebRequestTexture.GetTexture( _path ) )
+            {
+                www.method = UnityWebRequest.kHttpVerbGET;
+                using ( DownloadHandlerTexture handler = new DownloadHandlerTexture() )
+                {
+                    www.downloadHandler = handler;
+                    yield return www.SendWebRequest();
+
+                    if ( www.result == UnityWebRequest.Result.ConnectionError ||
+                         www.result == UnityWebRequest.Result.ProtocolError )
+                    {
+                        Debug.LogError( $"UnityWebRequest Error : {www.error}" );
+                        throw new System.Exception( $"UnityWebRequest Error : {www.error}" );
+                    }
+
+                    Texture2D tex = handler.texture;
+                    image.sprite = Sprite.Create( tex, new Rect( 0, 0, tex.width, tex.height ), new Vector2( .5f, .5f ), GameSetting.PPU, 0, SpriteMeshType.FullRect );
+
+                    float width = tex.width;
+                    float height = tex.height;
+
+                    float offsetX = ( float )Screen.width / tex.width;
+                    width *= offsetX;
+                    height *= offsetX;
+
+                    float offsetY = ( float )Screen.height / height;
+                    if ( offsetY > 1f )
+                    {
+                        width *= offsetY;
+                        height *= offsetY;
+                    }
+
+                    image.rectTransform.sizeDelta = new Vector2( width, height );
+                }
+            }
+        }
+    }
+}
