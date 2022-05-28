717484ef24a73a71917ae0faf2903a2800ddf150
wns <wnsghks2629@gmail.com>
2022-05-29 07:28:59 +0900

    Lobby LineSpectrum 개선 64개의 밴드버퍼를 이용하여 Audio 표현 값을 정규화하여 컬러계산하도록 변경

--- Assets/Scripts/UISys/Audio Visualizer/LineSpectrum.cs
+++ Assets/Scripts/UISys/Audio Visualizer/LineSpectrum.cs
@@ -6,53 +6,125 @@ public class LineSpectrum : MonoBehaviour
 {
     public AudioVisualizer audioVisualizer;
     public Transform spectrumPrefab;
-    private Transform[] visualSpectrums;
+    private Transform[] spectrums;
 
-    public int numSpectrum;
     public float specWidth;
     public float specBlank;
     public float specPower;
-
     public Color color = Color.white;
+    private int numSpectrum = 64;
+    private List<SpriteRenderer> specRenderer = new List<SpriteRenderer>();
+
+
+    // buffers
+    public float decreasePower = .005f;
+    private float[] freqBand64;
+    private float[] bandBuffer64;
+    private float[] bufferDecrease64;
 
     private void Awake()
     {
+        freqBand64 = new float[64];
+        bandBuffer64    = new float[64];
+        bufferDecrease64 = new float[64];
+
         // delegate chain
-        audioVisualizer.UpdateSpectrums += UpdateSpectrum;
+        audioVisualizer.UpdateSpectrums += UpdateBand64;
 
         // create spectrum objects
-        visualSpectrums = new Transform[numSpectrum];
+        spectrums = new Transform[numSpectrum];
         float offset   = specWidth + specBlank;
         float startPos = -( offset * numSpectrum * .5f );
-        for ( int i = 0; i < numSpectrum; ++i )
+        for ( int i = 0; i < numSpectrum; i++ )
         {
             Transform obj = Instantiate( spectrumPrefab, this.transform );
-            visualSpectrums[i] = obj.transform;
-            visualSpectrums[i].position = new Vector3( startPos + ( offset * i ), 0f, transform.position.z );
-            visualSpectrums[i].GetComponent<SpriteRenderer>().sortingOrder = ( int )transform.position.z;
+            spectrums[i] = obj.transform;
+            //spectrums[i].position = new Vector3( startPos + ( offset * i ), 0f, transform.position.z );
+            specRenderer.Add( spectrums[i].GetComponent<SpriteRenderer>() );
+            specRenderer[i].sortingOrder = ( int )transform.position.z;
 
             obj.GetComponent<SpriteRenderer>().color = color;
         }
     }
 
-    private void UpdateSpectrum( float[] _values, float _offset )
+    private void UpdateBand64( float[] _values, float _offset )
     {
-        float highValue = 1f;
-        for ( int i = 0; i < numSpectrum; i++ )
+        int count = 0;
+        int sampleCount =1;
+        int power = 0;
+
+        for ( int i = 0; i < 64; i++ )
         {
-            if ( highValue < _values[i] ) highValue = _values[i];
+            float average = 0f;
+            if ( i == 16 || i == 32 || i == 40 || i == 48 || i == 56 )
+            {
+                power++;
+                sampleCount = ( int )Mathf.Pow( 2, power );
+                if ( power == 3 )
+                     sampleCount -= 2;
+            }
+
+            for ( int j = 0; j < sampleCount; j++ )
+            {
+                average += _values[count] * ( count + 1 );
+                count++;
+            }
+
+            average /= count;
+            freqBand64[i] = average * 80;
+
+            BandBuffer( i );
         }
 
-        for ( int i = 0; i < numSpectrum; i++ )
+        UpdateSpectrum();
+    }
+    
+    private void BandBuffer( int _index )
+    {
+        if ( freqBand64[_index] > bandBuffer64[_index] )
         {
-            float value = ( _values[i] / highValue ) * 1000f * specPower * _offset;
-            // _values[i] * 1000f * specPower * _offset;
+            bandBuffer64[_index] = freqBand64[_index];
+            bufferDecrease64[_index] = decreasePower;
+        }
 
-            float y = visualSpectrums[i].localScale.y;
+        if ( freqBand64[_index] < bandBuffer64[_index] )
+        {
+            bandBuffer64[_index] -= bufferDecrease64[_index];
+            bufferDecrease64[_index] *= 1.2f;
+        }
+    }
+
+    private void UpdateSpectrum()
+    {
+        float bufferHighest = 0f;
+        for ( int i = 0; i < numSpectrum / 2; i++ )
+        {
+            if ( bandBuffer64[i] > bufferHighest )
+                 bufferHighest = bandBuffer64[i];
+        }
+
+        float offset = specWidth + specBlank;
+        for ( int i = 0; i < numSpectrum / 2; i++ )
+        {
+            BandBuffer( i );
+
+            float value = bandBuffer64[i] * specPower;
+
+            float y = spectrums[i].localScale.y;
             float scale = Mathf.Lerp( y, value, .35f );
 
             Vector3 newScale = new Vector3( specWidth, scale, 1f );
-            visualSpectrums[i].localScale = newScale;
+            spectrums[i].localScale                           = newScale; // left
+            spectrums[numSpectrum - 1 - i].localScale = newScale; // right
+
+            spectrums[i].position = new Vector3( -offset * i, 0f, transform.position.z );
+            spectrums[numSpectrum - 1 - i].position = new Vector3( offset * i, 0f, transform.position.z );
+
+            float normalizeBuffer = bandBuffer64[i] / bufferHighest;
+            normalizeBuffer = normalizeBuffer < .25f ? .25f : normalizeBuffer;
+            var newColor = new Color( normalizeBuffer, normalizeBuffer, normalizeBuffer, normalizeBuffer );
+            specRenderer[i].color                   = newColor; // left
+            specRenderer[numSpectrum - 1 - i].color = newColor; // right
         }
     }
 }
